f73a35b881782e194791df2477c51c65f02be6ca
JaronArmiger <jaron.armiger@gmail.com>
2020-05-03 18:54:11 -0500

    write working log_in_as test helper

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -13,11 +13,19 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   	assert_redirected_to new_user_session_path
   end
 
+  test "should create properly if user logged in" do
+  	log_in_as(users(:mathias))
+  	assert_difference 'Post.count', 1 do
+  		post posts_path params: { post: { content: "faut pas subir" } }
+  	end
+  end
+
   test "should redirect destroy when not logged in" do
   	assert_no_difference 'Post.count' do
   		delete post_path(@post)
   	end
   	assert_redirected_to new_user_session_path
   end
+  
 
 end
