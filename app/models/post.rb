60c70872a295e6e2de8f902c428bc6cf9414636f
JaronArmiger <jaron.armiger@gmail.com>
2020-05-02 19:56:49 -0500

    set up fixtures for comments, likes, posts, users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes, foreign_key: :liked_post_id
   has_many :likers, through: :likes
+
+  validates :content, presence: true, length: { maximum: 1000 }
 end
