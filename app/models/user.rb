27299f41f912b0471d88255ea8cad95e07340a21
JaronArmiger <jaron.armiger@gmail.com>
2020-05-02 19:31:00 -0500

    setup friendship association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, foreign_key: :liker_id
   has_many :liked_posts, through: :likes
+  has_and_belongs_to_many :friends,
+  						  class_name: "User",
+  						  join_table: :friendships,
+  						  foreign_key: :user_id,
+  						  association_foreign_key: :friend_user_id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_000942) do
+ActiveRecord::Schema.define(version: 2020_05_03_002256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_05_03_000942) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_user_id"
+    t.index ["friend_user_id", "user_id"], name: "index_friendships_on_friend_user_id_and_user_id", unique: true
+    t.index ["user_id", "friend_user_id"], name: "index_friendships_on_user_id_and_friend_user_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
