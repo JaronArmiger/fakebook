b217b2a252a79cc3b5b3e2a1678714d8227a3552
JaronArmiger <jaron.armiger@gmail.com>
2020-05-03 20:15:13 -0500

    write failing user edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,19 +4,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   def setup
   	@user = users(:mathias)
+  	log_in_as @user
   end
 
   test "unsuccessful edit" do
-  	log_in_as @user
   	get edit_user_path @user
   	assert_template 'users/edit'
   	patch user_path, params: { user: { name: "", 
   									   email: "", 
   									   password: "merde" } }
   	assert_template 'users/edit'
-  	assert_select 'div.alert', text: "the form contains 2 errors"
+  	assert_select 'div.alert', text: "the form contains 3 errors"
   end
 
   test "successful edit" do
+  	get edit_user_path @user
+  	assert_template 'users/edit'
+  	patch user_path, params: { user: { name: "Arlette", 
+  									   email: "arlette@ask.fr", 
+  									   password: "cettemerde" } }
+  	assert_template 'users/edit'
   end
 end
