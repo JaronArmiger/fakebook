8675d1cdfb47bf526501a8707164960b55e01f96
JaronArmiger <jaron.armiger@gmail.com>
2020-05-04 19:51:22 -0500

    implement deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+	before_action :correct_user, only: [:destroy]
 
 	def create
 		@comment = current_user.comments.build(comment_params)
@@ -17,11 +17,21 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
+		@comment.destroy
+		flash[:success] = "commentaire effacer"
+		redirect_to request.referrer || root_url
 	end
 
 	private
 
 		def comment_params
 			params.require(:comment).permit(:content, :post_id)
+		end
+
+		def correct_user
+			@comment = current_user.comments.find_by(id: params[:id])
+			if @comment.nil?
+				redirect_to request.referrer || root_url
+			end
 		end
 end
