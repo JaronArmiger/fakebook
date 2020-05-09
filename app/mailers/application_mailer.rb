1be4f433d4b126560ecc8ebd5591fdb7f442645a
JaronArmiger <jaron.armiger@gmail.com>
2020-05-09 18:41:13 -0500

    update environment config files with proper action_mailer settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'leconnard@ask.fr'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+	def sign_up(user)
+		@user = user
+		mail to: user.email, subject: "merci pour s'inscrire"
+	end
 end
