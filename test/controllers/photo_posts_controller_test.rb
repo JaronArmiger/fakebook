abfd1d5966a47163d09a1e309622428536a24746
JaronArmiger <jaron.armiger@gmail.com>
2020-05-10 17:58:00 -0500

    modify posts_interface integration test to test photo upload

--- test/controllers/photo_posts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PhotoPostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/integration/posts_interface_test.rb
+++ test/integration/posts_interface_test.rb
@@ -18,8 +18,9 @@ class PostsInterfaceTest < ActionDispatch::IntegrationTest
   	assert_select 'div#error_explanation'
 
   	#valid submission
+    image = fixture_file_upload('test/fixtures/juliette.jpg')
   	assert_difference 'Post.count', 1 do
-  		post posts_path, params: { post: { content: "ca veux dire?" } }
+  		post posts_path, params: { post: { content: "ca veux dire?", image: image } }
   	end
 
   end
