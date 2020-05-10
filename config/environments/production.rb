b0aeee6d480442028c8e3e58141f5bc4070fcb03
JaronArmiger <jaron.armiger@gmail.com>
2020-05-09 20:12:45 -0500

    fix typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password:  ENV['SENDGRID_PASSWORD'],
-    domain:    'enigmatic-hamlet-28976.herokuapp.com/.herokuapp.com',
+    domain:    'enigmatic-hamlet-28976.herokuapp.com/',
     address:   'smtp.sendgrid.net',
     port:      587,
     authentication: :plain,
